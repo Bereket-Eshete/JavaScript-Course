4156ab4186cdd41fdb65328ee73a4cde8997ecfe
Bereket-Eshete <bereketeshete63@gmail.com>
2025-05-28 12:48:53 +0300

    Complete Section: section1 object, array, function and typesOf function

---
+++ Variables/array.js
@@ -0,0 +1,8 @@
+let product_color = ["blue", "green", "red", "orange"];
+console.log(product_color);
+
+console.log(product_color[0]);
+product_color[0] = 42;
+console.log(product_color[0]);
+console.log(typeof product_color);
+console.log(product_color.length);
---
+++ Variables/object.js
@@ -0,0 +1,9 @@
+let course = {
+  name: "javaScript for Beginner",
+  hours: 3,
+};
+// console.log(course["name"]);
+course["name"] = "JavaScript 101";
+console.log(course["name"]);
+let property = "hours";
+console.log(course[property]);
