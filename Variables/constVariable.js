d75bfa26bad9acb3d844aabaa2fa09b98cc47644
Bereket-Eshete <bereketeshete63@gmail.com>
2025-05-28 12:54:28 +0300

    Complete Section: section1 object, array, function and typesOf function

--- Variables/constVariable.js
+++ Variables/constVariable.js
@@ -1,2 +1,3 @@
 const christmas_2025 = "12.26.2025";
 console.log(christmas_2025);
+const COLOR_GREEN = "green";
---
+++ Variables/primitiveType.js
@@ -0,0 +1,17 @@
+let favoriteFruit = "Appel";
+let favoriteCream = "checolate";
+let favoriteProgrmmingLanguage = `JavaScript`;
+
+let numberOfDonutes = 12;
+let pi = 3.14;
+let veryLargeNumber = 45356383475458n;
+let lovesCoding = true;
+let favoriteColor;
+console.log(favoriteColor);
+favoriteFruit = null;
+const uniqueKey = Symbol();
+
+let course = {
+  name: "javaScript for Beginner",
+  hours: 3,
+};
